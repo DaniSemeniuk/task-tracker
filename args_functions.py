e27e52a0accf16186f027ed8f4ce76e4d23b0687
Danilo Semeniuk <danisemeniuk13@gmail.com>
2025-08-19 16:21:27 -0300

    add entry point guard and merged functions
    merged arguments function out of main

--- args_functions.py
+++ args_functions.py
@@ -1,13 +1,60 @@
 """functions for the CLI arguments"""
 
 import json
+import os
+from datetime import datetime
 
 
-def dump(data):
-    with open("DB.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
-    return
+def dump(data, filename="DB.json"):
+    temp_file = filename + ".tmp"
+    try:
+        with open(temp_file, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+        os.replace(temp_file, filename)  # reemplaza atómicamente el archivo
+    except Exception as e:
+        if os.path.exists(temp_file):
+            os.remove(temp_file)  # borramos el temporal si algo falló
+        raise e
 
 
-def add():
-    pass
+def add(data, args):
+    data["tasks"][str(data["count"])] = {
+        "description": args.add,
+        "status": "to-do",
+        "createdAt": datetime.now().isoformat(),
+        "updateAt": datetime.now().isoformat(),
+    }
+    data["count"] += 1
+    dump(data)
+
+
+def display(data, args):
+    listing = []
+    for key_id, task in data["tasks"].items():
+        if args.list in (task["status"], "all"):
+            listing.append(
+                f"ID: {key_id}\n"
+                f"Description: {task['description']}\n"
+                f"Status: {task['status']}\n"
+                f"Created at: {datetime.fromisoformat(task['createdAt']).strftime('%d-%m-%Y %H:%M:%S')}\n"
+                f"Updated at: {datetime.fromisoformat(task['updateAt']).strftime('%d-%m-%Y %H:%M:%S')}\n\n"
+            )
+    if not listing:
+        print("There is no tasks :(")
+    else:
+        for task in listing:
+            print(task)
+
+
+def update(data, args):
+    task_id = args.update[0]
+    task_desc = args.update[1]
+    if not task_id.isnumeric():
+        raise ValueError("Usage: --update id[int] new_task[str]")
+
+    try:
+        data["tasks"][task_id]["description"] = task_desc
+        data["tasks"][task_id]["updateAt"] = datetime.now().isoformat()
+        dump(data)
+    except KeyError:
+        print("That id doesn't exist")
--- main.py
+++ main.py
@@ -2,88 +2,61 @@
 
 import argparse
 import json
-from datetime import datetime
+import sys
 import args_functions
 
 
-# creation of argument parser
-parser = argparse.ArgumentParser(description="task manager in JSON file")
+def main(args):
+    # basic data structure
+    data = {"count": 1, "tasks": {}}
 
-# add exclusive arguments
-exclusive_args = parser.add_mutually_exclusive_group()
-exclusive_args.add_argument("--add", help="add a new task: add 'task'[str]")
-exclusive_args.add_argument(
-    "--update", nargs=2, help="update an existing task: update id[int] 'new task'[str]"
-)
-exclusive_args.add_argument("--delete", help="delete a task: delete id[int]")
-exclusive_args.add_argument(
-    "--list",
-    choices=["all", "done", "to-do", "in-progress"],
-    help="list tasks: list 'all'/'done'/'to-do'/'in-progress'[str]",
-)
-exclusive_args.add_argument(
-    "--mark",
-    choices=["in-progress", "done", "to-do"],
-    help="change task status: mark 'status'[str]",
-)
-
-# parse arguments
-args = parser.parse_args()
-
-# basic data structure
-data = {"count": 1, "tasks": {}}
-
-# loading or creating JSON data base
-try:
-    with open("DB.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-except FileNotFoundError as e:
-    with open("DB.json", "x", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-
-# ADD funcionality
-if args.add:
-    data["tasks"][str(data["count"])] = {
-        "description": args.add,
-        "status": "to-do",
-        "createdAt": datetime.now().isoformat(),
-        "updateAt": datetime.now().isoformat(),
-    }
-    data["count"] += 1
-    args_functions.dump(data)
-    exit(1)
-
-# LIST funcionality
-if args.list:
-    listing = []
-    for key_id, task in data["tasks"].items():
-        if args.list in (task["status"], "all"):
-            listing.append(
-                f"ID: {key_id}\n"
-                f"Description: {task['description']}\n"
-                f"Status: {task['status']}\n"
-                f"Created at: {datetime.fromisoformat(task['createdAt']).strftime('%d-%m-%Y %H:%M:%S')}\n"
-                f"Updated at: {datetime.fromisoformat(task['updateAt']).strftime('%d-%m-%Y %H:%M:%S')}\n\n"
-            )
-    if not listing:
-        print("There is no tasks with that status :(")
-    else:
-        for task in listing:
-            print(task)
-    exit(1)
-
-# UPDATE funcionality
-if args.update:
-    task_id = args.update[0]
-    task_desc = args.update[1]
-    if not task_id.isnumeric():
-        raise ValueError("Usage: --update id[int] new_task[str]")
-
+    # loading or creating JSON data base
     try:
-        data["tasks"][task_id]["description"] = task_desc
-        data["tasks"][task_id]["updateAt"] = datetime.now().isoformat()
-        with open("DB.json", "w", encoding="utf-8") as f:
-            args_functions.dump(data)
-    except KeyError:
-        print("That id doesn't exist")
-        exit(1)
+        with open("DB.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        with open("DB.json", "x", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
+    # ADD funcionality
+    if args.add:
+        args_functions.add(data, args)
+        sys.exit()
+
+    # LIST funcionality
+    if args.list:
+        args_functions.display(data, args)
+        sys.exit()
+
+    # UPDATE funcionality
+    if args.update:
+        args_functions.update(data, args)
+        sys.exit()
+
+
+if __name__ == "__main__":
+    # creation of argument parser
+    parser = argparse.ArgumentParser(description="task manager in JSON file")
+
+    # add exclusive arguments
+    exclusive_args = parser.add_mutually_exclusive_group()
+    exclusive_args.add_argument("--add", help="add a new task: add 'task'[str]")
+    exclusive_args.add_argument(
+        "--update",
+        nargs=2,
+        help="update an existing task: update id[int] 'new task'[str]",
+    )
+    exclusive_args.add_argument("--delete", help="delete a task: delete id[int]")
+    exclusive_args.add_argument(
+        "--list",
+        choices=["all", "done", "to-do", "in-progress"],
+        help="list tasks: list 'all'/'done'/'to-do'/'in-progress'[str]",
+    )
+    exclusive_args.add_argument(
+        "--mark",
+        choices=["in-progress", "done", "to-do"],
+        help="change task status: mark 'status'[str]",
+    )
+
+    # parse arguments
+    main(parser.parse_args())
