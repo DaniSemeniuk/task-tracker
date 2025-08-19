24beff6b3264e1b5f1598c60453bdc0f07f908fd
Danilo Semeniuk <danisemeniuk13@gmail.com>
2025-08-18 21:19:31 -0300

    add list funcionality

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import json
 from datetime import datetime
 
 
-###creation of exclusive CLI commands###
+# creation of argument parser
 parser = argparse.ArgumentParser(description="task manager in JSON file")
+
+# add exclusive arguments
 exclusive_args = parser.add_mutually_exclusive_group()
 exclusive_args.add_argument("--add", help="add a new task: add 'task'[str]")
 exclusive_args.add_argument(
@@ -13,36 +15,57 @@ exclusive_args.add_argument(
 exclusive_args.add_argument("--delete", help="delete a task: delete id[int]")
 exclusive_args.add_argument(
     "--list",
-    choices=[None, "done", "todo", "in-progress"],
-    help="list tasks: list 'done'/'todo'/'in-progress'[str] | None",
+    choices=["all", "done", "to-do", "in-progress"],
+    help="list tasks: list 'all'/'done'/'to-do'/'in-progress'[str]",
 )
 exclusive_args.add_argument(
     "--mark",
     choices=["in-progress", "done", "to-do"],
     help="change task status: mark 'status'[str]",
 )
-###parsin arguments###
+
+# parse arguments
 args = parser.parse_args()
 
-
+# basic data structure
 data = {"count": 1, "tasks": {}}
 
+# loading or creating JSON data base
 try:
     with open("DB.json", "r", encoding="utf-8") as f:
         data = json.load(f)
-        print(data)
 except FileNotFoundError as e:
     with open("DB.json", "x", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
+# ADD funcionality
 if args.add:
-    data["tasks"][data["count"]] = {
+    data["tasks"][str(data["count"])] = {
         "description": args.add,
         "status": "to-do",
         "createdAt": datetime.now().isoformat(),
         "updateAt": datetime.now().isoformat(),
     }
     data["count"] += 1
-    print(json.dumps(data, indent=4, ensure_ascii=False))
     with open("DB.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+        json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
+    exit(1)
+
+# LIST funcionality
+if args.list:
+    listing = []
+    for key_id, task in data["tasks"].items():
+        if args.list in (task["status"], "all"):
+            listing.append(
+                f"ID: {key_id}\n"
+                f"Description: {task['description']}\n"
+                f"Status: {task['status']}\n"
+                f"Created at: {datetime.fromisoformat(task['createdAt']).strftime('%d-%m-%Y %H:%M:%S')}\n"
+                f"Updated at: {datetime.fromisoformat(task['updateAt']).strftime('%d-%m-%Y %H:%M:%S')}\n\n"
+            )
+    if not listing:
+        print("There is no tasks with that status :(")
+    else:
+        for task in listing:
+            print(task)
+    exit(1)
